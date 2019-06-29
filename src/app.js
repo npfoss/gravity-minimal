382122c6937235b3fb411788215c67130a45744a
Nate Foss <npfoss@gmail.com>
2019-06-29 15:24:27 -0400

    add refresh and profile view

--- src/app.js
+++ src/app.js
@@ -3,6 +3,14 @@
 // this is the wrong place to import from, replace with npm import later (TODO)
 const GravityProtocol = require('../../gravity-protocol') // two ..s because it ends up in public/
 
-console.log('doing the thing')
 
 const gp = new GravityProtocol()
+
+document.getElementById("refresh").addEventListener("click", function(){
+	gp.loadFiles('/')
+		.then(resp => {
+			document.getElementById("profile").innerHTML = JSON.stringify(resp, null, '\t')
+		})
+});
+
+
