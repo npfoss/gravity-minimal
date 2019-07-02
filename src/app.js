306ceaed90c68ff3c89b159fd758abea05e2eb68
Nate Foss <npfoss@gmail.com>
2019-07-01 23:27:59 -0400

    test keygen storage and retrieval

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 'use strict'
 
 // this is the wrong place to import from, replace with npm import later (TODO)
-const GravityProtocol = require('../../gravity-protocol') // two ..s because it ends up in public/
+const GravityProtocol = require('../../gravity-protocol'); // two ..s because it ends up in public/
 
 
-const gp = new GravityProtocol()
+const gp = new GravityProtocol();
 
 document.getElementById("refresh").addEventListener("click", function(){
 	// another example to try: /ipfs/QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG
 	gp.loadDirs(document.getElementById("pathinput").value)
 		.then(resp => {
-			document.getElementById("profile").innerHTML = JSON.stringify(resp, null, '\t')
-		})
+			document.getElementById("profile").innerHTML = JSON.stringify(resp, null, '\t');
+		});
 });
 
+document.getElementById("keygen").addEventListener("click", function(){
+	gp.resetMasterKey();
+})
 
+document.getElementById("getkey").addEventListener("click", function(){
+	console.log(gp.getMasterKey());
+})
