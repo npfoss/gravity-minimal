f1d96deac2567791dbe4aa4586c7417c04b6b1f1
Nate Foss <npfoss@gmail.com>
2019-07-18 09:15:20 -0400

    nickname test

--- src/app.js
+++ src/app.js
@@ -66,3 +66,13 @@ document.getElementById("creategroup").addEventListener("click", async () => {
 	gp.createGroup(Object.keys(c)) // creates a group with all contacts
 		.then(res => {console.log(res)})
 })
+
+document.getElementById("setnick").addEventListener("click", async () => {
+	let groups = await gp.getGroupList();
+	console.log(groups)
+	console.log(await getGroupInfo(groups[0]))
+	let nicks = {};
+	nicks[document.getElementById("nickkey").value] = document.getElementById("nickname").value;
+	await gp.setNicknames(nicks, groups[0])
+	console.log(await getGroupInfo(groups[0]))
+});
